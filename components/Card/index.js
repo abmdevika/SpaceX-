2439f1ae0da501c896efae05d73a3d48e9ec8d3e
devika <abmdevika@gmail.com>
2020-08-14 17:33:06 +0530

    responssive ui related changes

--- components/Card/index.js
+++ components/Card/index.js
@@ -11,20 +11,29 @@ export default function Card({
   return (
     <div className={styles.Card}>
       <img src={links.mission_patch} alt={mission_name} width='256' />
-      <h5>
-        {mission_name} #{flight_number}
-      </h5>
-      <label>mission_id:</label>
-      <ul>
-        {mission_id.map((missionId) => (
-          <li key={missionId}>{missionId}</li>
-        ))}
-      </ul>
-      <ul>
-        <li>launch_year: {launch_year}</li>
-        <li> launch_success: {launch_success ? 'true' : 'false'}</li>
-        <li> land_success: {mission_id.length > 0 ? 'true' : 'false'}</li>
-      </ul>
+      <div>
+        <h5>
+          {mission_name} #{flight_number}
+        </h5>
+        <label>mission_id:</label>
+        <ul>
+          {mission_id.map((missionId) => (
+            <li key={missionId}>{missionId}</li>
+          ))}
+        </ul>
+        <ul className={styles.launchDetails}>
+          <li>
+            <span>launch_year:</span> {launch_year}
+          </li>
+          <li>
+            <span>launch_success:</span> {launch_success ? 'true' : 'false'}
+          </li>
+          <li>
+            <span>land_success: </span>
+            {mission_id.length > 0 ? 'true' : 'false'}
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
--- components/Tile/index.js
+++ components/Tile/index.js
@@ -1,8 +1,10 @@
 import styles from './Tile.module.scss';
 
-export default function Tile({ label, onTileClick }) {
+export default function Tile({ label, onTileClick, isActive }) {
+  const { Tile, active } = styles;
+
   return (
-    <span className={styles.Tile} onClick={() => onTileClick()}>
+    <span className={isActive ? active : Tile} onClick={() => onTileClick()}>
       {label}
     </span>
   );
